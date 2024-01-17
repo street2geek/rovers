59b98a468ddb3b809ed09fbf4c9d7bc39767557e
Wafiq Baugh <screenlantern@gmail.com>
2024-01-17 23:11:36 +0000

    amend

--- main_test.ts
+++ main_test.ts
@@ -4,6 +4,6 @@ import { init } from "./main.ts";
 const mockCommandInput = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM";
 const mockExpectedOutput = "1 3 N\n5 1 E";
 
-Deno.test("initialized mission output", () => {
+Deno.test("init() mission output", () => {
   assertEquals(init(mockCommandInput), mockExpectedOutput);
 });
